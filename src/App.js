b691b92b0d4849aaf722412a7b3df5c07169e795
devarchi33 <devarchi33@gmail.com>
2016-11-14 14:54:26 +0900

    Apply state property.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
 import React, {Component} from 'react';
 
 class App extends Component {
+    constructor() {
+        super();
+        this.state = { message : "This is default state."}
+    }
+
+    update(e) {
+        this.setState({ message : e.target.value })
+    }
+
     render () {
         let name = this.props.name;
 
         return  (
             <div>
                 <h1>Hello, {name}</h1>
+
+                <input type="text" onChange={this.update.bind(this)}/>
+                <h2>{this.state.message}</h2>
             </div>
         )
     }
